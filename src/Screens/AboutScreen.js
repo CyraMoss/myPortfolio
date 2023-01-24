9991b126f0bee66af9f41f3477e84c8c2dff0e99
CyraMoss <cyramoss@icloud.com>
2023-01-24 23:17:59 +1300

    cancelled

--- src/Screens/AboutScreen.js
+++ src/Screens/AboutScreen.js
@@ -2,21 +2,15 @@ import React from 'react';
 
 export default function AboutScreen() {
   return (
-    <div className="center blur border margin10">
-      <h1>About Me</h1> <hr />
-      <p>
+    <div className=" blur border margin10">
+      <h1 className="center">About Me</h1> <hr />
+      <p className="aboutp">
         Hi! my name is Cyra, i'm a 26 years old Front-End software Developer
         from Auckland, New Zealand. <br />
         When I started thinking about what I wanted to do for a career I thought
         about something to do with computers straight away. I was trying to find
         something I would enjoy so I tried a level 3 computing course and liked
         it so much I did level 4 & 5 aswell.
-        <br />
-        After those I thought I'd try courses on udemy to see what I was more
-        interested in and found that i liked to code! to me seeing something I
-        programmed to work is fun and to make it look exactly like how I want. I
-        do struggle sometimes with bugs but that's how I get to learn more
-        reseaching how to fix it.
         <br />
         When I decided to do software development I came across a course called
         SheCodes, so I signed up!
